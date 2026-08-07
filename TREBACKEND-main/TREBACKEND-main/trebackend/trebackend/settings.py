3162de964f85cfdf4e7074d5c23d444ca56e2db7
vaibhav91-singh <singhvaibhav849@gmail.com>
2026-08-08 00:07:13 +0530

    Add dj-database-url for cloud PostgreSQL integration

--- TREBACKEND-main/TREBACKEND-main/trebackend/trebackend/settings.py
+++ TREBACKEND-main/TREBACKEND-main/trebackend/trebackend/settings.py
@@ -79,12 +79,14 @@ WSGI_APPLICATION = 'trebackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
