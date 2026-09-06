fcb89a08f943c4569d75e273083eb06a8fe50952
vaibhav91-singh <singhvaibhav849@gmail.com>
2026-09-07 05:06:52 +0530

    fix: remove conn_health_checks from settings.py for compatibility with dj_database_url 0.5.0

--- TREBACKEND-main/TREBACKEND-main/trebackend/trebackend/settings.py
+++ TREBACKEND-main/TREBACKEND-main/trebackend/trebackend/settings.py
@@ -95,7 +95,6 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
-        conn_health_checks=True,
     )
 }
 
